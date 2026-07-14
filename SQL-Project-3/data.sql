38e128fb1e4fe18b90e158427a77fedada3700eb
Sharnitharaj <sharnithat@gmail.com>
2026-07-15 00:51:05 +0530

    Add files via upload

---
+++ SQL-Project-3/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Orders VALUES
+('ORD200000',44930,'C72649','Monitor',5,570.62,'928 Main St','Debit Card','Shipped','TRK37947903',7,'SAVE10','Instagram',2853.10),
+('ORD200001',45527,'C75739','Phone',2,151.35,'823 Main St','Online','Shipped','TRK91186779',3,'SAVE10','Referral',302.70),
+('ORD200002',45349,'C81728','Tablet',5,550.68,'512 Main St','Credit Card','Cancelled','TRK42903982',8,'FREESHIP','Email',2753.40),
+('ORD200003',45214,'C33540','Chair',1,273.19,'275 Main St','Debit Card','Returned','TRK62788070',5,'SAVE10','Facebook',273.19);
